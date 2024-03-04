af80db5cba317c78620c31b8f32c6a025ed76bc5
mahnoor-shakil <mah.realone@gmail.com>
2024-03-04 14:02:42 +0500

    add one html and one css file

---
+++ app.js
@@ -0,0 +1,136 @@
+// chapter 9,10,11
+
+// q1)
+// var cityname = prompt("enter city name");
+// if (cityname === "karachi") {
+//     alert ("Welcome to city of lights")
+// }
+// else {
+//     alert ("welcome to another city")
+// }
+
+// q2
+// var gender = prompt("enter your gender");
+// if (gender === "female") {
+//     alert ("Good Morning Ma'am")
+// }
+// else {
+//     alert ("Good Morning Sir")
+// }
+
+// q3
+// var signals = prompt("enter color");
+// if (signals === "red") {
+//     alert ("Must Stop")
+// }
+// else if (signals === "yellow") {
+//     alert ("Ready to move")
+// }
+// else if (signals === "green") {
+//     alert ("move now")
+// }
+// else {
+//     alert("sleep")
+// }
+
+
+// q4
+// var fuel = prompt("enter your value");
+// if (fuel > "0.25liters") {
+//     alert ("please refill the fuel in your car")
+// }
+// else {
+//     alert ("enough fuel")
+// } 
+
+// q6
+// document.write ("<b>Mark Sheet</B>")
+// var urdu = +prompt("Enter marks");
+// var english = +prompt("Enter marks");
+// var maths = +prompt("Enter marks");
+// var totalmarks = +prompt("enter totalmarks")
+// var totalobtainedmarks = urdu + english +maths;
+// var percentage = (totalobtainedmarks / (3 * totalMarks)) * 100;
+// var grade;
+// var remarks;
+// if (percentage >= 80) {
+//    console.log("total marks : 300" + "<br>" + "marks obtained :"+ totalobtainedmarks+ "<br>" + "percentage :" + percentage + "<br>"+ "grade:"+ "A-one" +"<br>"+ "remarks:"+"Excellent")
+// } else if (percentage >= 70) {
+//     console.log("total marks : 300" + "<br>" + "marks obtained :"+ totalobtainedmarks+ "<br>" + "percentage :" + percentage + "<br>"+ "grade:"+ "A-" +"<br>"+ "remarks:"+"good")
+
+// } else if (percentage >= 60) {
+//     console.log("total marks : 300" + "<br>" + "marks obtained :"+ totalobtainedmarks+ "<br>" + "percentage :" + percentage + "<br>"+ "grade:"+ "B" +"<br>"+ "remarks:"+"yOU NEED TO IMPROVE")
+
+// }
+//  else {
+//     console.log("total marks : 300" + "<br>" + "marks obtained :"+ totalobtainedmarks+ "<br>" + "percentage :" + percentage + "<br>"+ "grade:"+ "FAIL" +"<br>"+ "remarks:"+"Sorry")
+
+// }
+
+
+// q7
+// var secretnumber = 8;
+// var guessnumber = +prompt("guess secret number");
+// if (guessnumber == secretnumber){
+//     document.write ("Bingo! Corret answer")
+// }
+// else {
+//     document.write ("close enough to the correct answer")
+// }
+
+// q8
+// var number = +prompt("enter number")
+// if (number % 3 === 0) {
+//     document.write(number + " is divisible by 3.");
+// } else {
+//     document.write(number + " is not divisible by 3.");
+// }
+
+// q9
+// var number = +prompt("enter number")
+// if (number % 2 === 0) {
+//     document.write(number + " is an even number.");
+// } else {
+//     document.write(number + " is an odd number.");
+// }
+
+// q10
+// var temperature = +prompt("write temperature");
+// if (temperature > 40) {
+//     document.write("It is too hot outside")
+// }
+// else if (temperature > 30) {
+//     document.write("The weather today is normal")
+// }
+// else if (temperature > 20) {
+//     document.write("Todays weather is cool")
+// }
+// else if (temperature > 10) {
+//     document.write("“OMG! Today’s weather is so Cool.")
+// }
+// else{
+//     document.write ("none")
+// }
+
+// q11
+
+// var num1 = prompt("Enter first number:");
+// var num2 = prompt("Enter second number:");
+// var operation = prompt("Enter operation (+, -, *, /, %):");
+// var result;
+
+
+// if (operation === '+') {
+//     document.write = (num1 + num2);
+// } else if (operation === '-') {
+//     document.write = (num1 - num2);
+// } else if (operation === '*') {
+//     document.write = (num1 * num2);
+// } else if (operation === '/') {
+//     document.write= (num1 / num2);
+// } else if (operation === '%') {
+//     document.write = (num1 % num2);
+// } else {
+//     document.write = ("Invalid operation");
+// }
+
